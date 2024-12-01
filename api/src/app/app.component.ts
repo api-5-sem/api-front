f962dfc289483386d29dc7435cd7943daa130086
Matheus Fiebig <macrubig@gmail.com>
2024-12-01 14:17:18 -0300

    PX-142 - Correção testes unitarios

--- api/src/app/app.component.ts
+++ api/src/app/app.component.ts
@@ -16,9 +16,9 @@ export class AppComponent implements OnInit , OnDestroy{
 
     ngOnInit(): void {
         const headers = new HttpHeaders().set('Authorization', `${this.tokenAuth}`);
-    
+
         this.notificationInterval = setInterval(() => {
-            //this.http.post(environment.apiUrl + 'notificacoes', { usuario: 'admin' },{headers}).subscribe()
+            this.http.post(environment.apiUrl + 'notificacoes', { usuario: 'admin' },{headers}).subscribe()
         }, 15000)
     }
 
